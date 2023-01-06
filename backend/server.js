7b363e96143c8b69079cdacc10632f2a36e41dca
aladdin <ismael.aladdin@gmail.com>
2023-01-05 23:00:13 -0800

    tinkering with cors origin part 4

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,42 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(
-  cors({
-    origin: "http://sample.tube",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://sample.tube",
+//   })
+// );
+
+// Set up a whitelist and check against it:
+var whitelist = [
+  "http://sample.tube",
+  "http://www.sample.tube",
+  "http://sampletube.netlify.app",
+];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+// Then pass them to cors:
+app.use(cors(corsOptions));
+
+// app.use((req, res, next) => {
+//     const allowedOrigins = ['http://sample.tube', 'http://sampletube.netlify.app'];
+//     const origin = req.headers.origin;
+//     if (allowedOrigins.includes(origin)) {
+//          res.setHeader('Access-Control-Allow-Origin', origin);
+//     }
+//     //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+//     res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.header('Access-Control-Allow-Credentials', true);
+//     return next();
+//   });
 
 app.use("/api/videos", require("./routes/videoRoutes"));
 
