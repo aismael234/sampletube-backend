df8570f907970041a683314eb2c237fecb6eff8d
aladdin <ismael.aladdin@gmail.com>
2023-01-05 18:17:22 -0800

    Finalize MVP for website backend

--- backend/controllers/videoController.js
+++ backend/controllers/videoController.js
@@ -9,53 +9,57 @@ const getVideo = asyncHandler(async (req, res) => {
 
   // Change date (string form) format
   // from yyyy-mm-dd to yyyymmdd to match database format
-  let date_from;
-  let date_to;
-  if (req.query.date_from) date_from = req.query.date_from.replaceAll("-", "");
-  if (req.query.date_to) date_to = req.query.date_to.replaceAll("-", "");
+  const queryData = req.query;
 
-  console.log(req.query);
+  let andArray = [];
 
-  const video = await Video.aggregate([
-    {
-      $match: {
-        $and: [
-          {
-            $and: [
-              {
-                $and: [
-                  {
-                    view_count: { $gte: parseInt(req.query.minimum_views, 10) },
-                  },
-                  {
-                    view_count: { $lte: parseInt(req.query.maximum_views, 10) },
-                  },
-                ],
-              },
-              {
-                $and: [
-                  {
-                    upload_date: {
-                      $gte: req.query.date_from.replaceAll("-", ""),
-                    },
-                  },
-                  {
-                    upload_date: {
-                      $lte: req.query.date_to.replaceAll("-", ""),
-                    },
-                  },
-                ],
-              },
-            ],
-          }, //use $in tag for multiple keywords? (would have OR behavior i believe)
-          { title: { $regex: "for", $options: "i" } },
-        ],
+  // if no queries exist. Usually upon intial page load.
+  if (queryData != false) {
+    const video = await Video.aggregate([{ $sample: { size: 1 } }]);
+
+    res.status(200).json(video);
+  } else {
+    // adds all non-empty values to an array to be later used into a query
+    // "false" condition accounts for "", null, and undefined (kinda lazy/irresponsible, I know.).
+    if (queryData.minimum_views != false)
+      andArray.push({
+        view_count: { $gte: parseInt(req.query.minimum_views, 10) },
+      });
+    if (queryData.maximum_views != false)
+      andArray.push({
+        view_count: { $lte: parseInt(req.query.maximum_views, 10) },
+      });
+    if (queryData.date_from != false)
+      andArray.push({
+        upload_date: {
+          $gte: req.query.date_from.replaceAll("-", ""),
+        },
+      });
+    if (queryData.date_to != false)
+      andArray.push({
+        upload_date: {
+          $lte: req.query.date_to.replaceAll("-", ""),
+        },
+      });
+    if (queryData.title != false)
+      andArray.push({
+        title: { $regex: req.query.keywords, $options: "i" },
+      });
+
+    // takes in the whole andArray and uses all non-empty values to query
+    const video = await Video.aggregate([
+      {
+        $match: {
+          $and: andArray,
+        },
       },
-    },
-    { $sample: { size: 1 } },
-  ]);
-  console.log(video);
-  res.status(200).json(video);
+      { $sample: { size: 1 } },
+    ]);
+
+    //console.log(video);
+
+    res.status(200).json(video);
+  }
 });
 
 // @desc Updates the like/dislike count of a video object
